09e8af13138ec8a60d7ba770bab4ab4a56131b70
Guilherme Amaral <gvma@ic.ufal.br>
2022-06-08 14:17:50 -0300

    Hard coded the database credentials

--- src/db/config.ts
+++ src/db/config.ts
@@ -4,7 +4,7 @@ require('dotenv').config();
 let sequelizeConnection: any;
 
 if (process.env.ENVIRONMENT === 'dev') {
-	const dbName: string = process.env.DB_NAME_DEV!;
+	const dbName = process.env.DB_NAME_DEV!;
 	const dbHost = process.env.DB_HOST_DEV!;
 	const dbUser = process.env.DB_USER_DEV!;
 	const dbPassword = process.env.DB_PASS_DEV!;
@@ -13,10 +13,10 @@ if (process.env.ENVIRONMENT === 'dev') {
 		dialect: 'postgres'
 	})
 } else if (process.env.ENVIRONMENT === 'test') {
-	const dbName: string = process.env.DB_NAME_TEST!;
-	const dbHost = process.env.DB_HOST_TEST!;
-	const dbUser = process.env.DB_USER_TEST!;
-	const dbPassword = process.env.DB_PASS_TEST!;
+	const dbName = 'dannneetvn6vrd';
+	const dbHost = 'ec2-52-72-99-110.compute-1.amazonaws.com';
+	const dbUser = 'hoowrqwrbqcfci';
+	const dbPassword = '17c3d91644cb7ac127e7931b38c5df78eeb3fac75e1f73b919bcd56a5214eef6';
 	sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
 		host: dbHost,
 		dialect: 'postgres'
